6b8843a0cb934c9d131a626c3e4f7fab1f768aae
Matija Komorcec <matija.komorcec@mogy.me>
2016-10-13 23:13:44 +0200

    Updated the reducer.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Twitch channel search
 
-This is a simple app that demonstrates React and Redux with a Twitch API.
+This is a simple app that demonstrates React and Redux with a Twitch API. It will be a part of a tutorial on my personal website - http://matijakomorcec.com
--- src/Reducers/reducer.js
+++ src/Reducers/reducer.js
@@ -1,38 +1,41 @@
 export function reducer (state = { isSearching: false, channelData: {}, channelError: '' }, action) {
     switch(action.type) {
         case 'SEARCH_CHANNEL_STARTED':
-            state = Object.assign(
-                {}, 
-                state.isSearching, 
-                { 
-                    isSearching: true,
-                    channelData: {},
-                    channelError: ""
-                }
+            return (
+                Object.assign(
+                    {}, 
+                    state, 
+                    { 
+                        isSearching: action.isSearching
+                    }
+                )
             );
-            return state;
+
         case 'SEARCH_CHANNEL_SUCCESS':
-            state = Object.assign(
-                {},
-                state.isSearching, 
-                { 
-                    isSearching: false,
-                    channelData: action.channelData.data,
-                    channelError: "" 
-                }
-            );
-            return state;
+            return (   
+                Object.assign(
+                    {},
+                    state, 
+                    { 
+                        isSearching: action.isSearching,
+                        channelData: action.channelData.data,
+                        channelError: ''
+                    }
+                )
+            );      
+
         case 'SEARCH_CHANNEL_ERROR':
-            state = Object.assign(
-                {}, 
-                state, 
-                { 
-                    isSearching: false,
-                    channelData: {},
-                    channelError: action.channelError.message 
-                }
+            return (
+                Object.assign(
+                    {}, 
+                    state, 
+                    { 
+                        isSearching: action.isSearching,
+                        channelError: action.channelError.message 
+                    }
+                )
             );
-            return state;
+
         default:
             return state;
     }
